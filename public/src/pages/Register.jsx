9145c6b713af49170908049b7a34b657c297bb98
harshit <verma8497@gmail.com>
2023-01-29 18:09:02 +0530

    Register Functionality Done

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,59 +1,119 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
-
-const handleSubmit = (event)=>{
-  event.preventDefault();
-  alert("form");
-};
-
-const handleChange = (event)=>{
-
-};
+import {ToastContainer,toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from "../utils/API";
 
 export default function Register() {
+
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const toastOptions = {position: "bottom-right",
+  autoClose: 8000,
+  pauseOnHover: true,
+  draggable: true,
+  theme: "dark",
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if(handleValidation()){
+      //Call API
+      console.log("validated");
+      const {password,username,email} = values;
+      const {data} = await axios.post(registerRoute,{
+        username,
+        email,
+        password,
+      });
+      if(data.status === false){
+        toast.error(data.msg,toastOptions);
+      }
+      else{
+        localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+        navigate("/");
+      }
+    }
+  };
+
+
+  const handleValidation = ()=>{
+    const {password,confirmPassword,username,email} = values;
+    if(password !== confirmPassword){
+      toast.error("Password and confirm Password are not same",toastOptions);
+      return false;
+    }
+    else if (username.length < 3){
+      toast.error("Username must be atleast 3 length size",toastOptions);
+      return false;
+    }
+    else if (password.length < 8){
+      toast.error("Password must be atleast 8 length size",toastOptions);
+      return false;
+    }
+    else if (email === ""){
+      toast.error(" Email required ",toastOptions);
+      return false;
+    }
+    return true;
+  };
+  
+  const handleChange = (event) => {
+    setValues({...values, [event.target.name]: event.target.value});
+  };
+
   return (
     <div>
       <FormContainer>
-        <form onSubmit={(event)=>handleSubmit(event)}>
-          <div className='brand'>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
             <img src={Logo} alt="Logo" />
             <h1>snappy</h1>
           </div>
-          <input 
-            type="text" 
-            placeholder='Username' 
-            name='username' 
-            onChange={e=> handleChange(e) } 
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
           />
-          <input 
-            type="email" 
-            placeholder='Email' 
-            name='email' 
-            onChange={e=> handleChange(e) } 
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
           />
-          <input 
-            type="password" 
-            placeholder='Password' 
-            name='password' 
-            onChange={e=> handleChange(e) } 
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
           />
-          <input 
-            type="password" 
-            placeholder='Re-enter Password' 
-            name='confirmPassword' 
-            onChange={e=> handleChange(e) } 
+          <input
+            type="password"
+            placeholder="Re-enter Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
           />
-          <button type='submit'>Create User</button>
+          <button type="submit">Create User</button>
           <span>
             Already have Account ? <Link to="/login">Login</Link>
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </div>
-  )
-};
+  );
+}
 
 const FormContainer = styled.div`
   height: 100vh;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
 
 const app = express();
@@ -9,6 +10,8 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", userRoutes);
 
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.MONGO_URL,{
