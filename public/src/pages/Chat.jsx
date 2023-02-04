20e01e59b06e24fbd9375fb89ca6f3daf24c1b5e
harshit <verma8497@gmail.com>
2023-02-05 00:25:19 +0530

    Chat Container Setup done

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { Buffer } from "buffer";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute, setAvatarRoute } from "../utils/API";
+import Contacts from "../components/Contacts";
+
+
 
 export default function Chat() {
+
+  const navigate = useNavigate();
+
+  const [contacts, setContacts] = useState([]);
+
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    async function func() {
+      if (!localStorage.getItem("chat-app-user")) {
+        navigate("/login");
+      }
+      else{
+        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+      }
+    }
+    func();
+  }, []);
+
+  useEffect(()=>{
+    async function func() {
+      if(currentUser){
+        if(currentUser.isAvatarImageSet){
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        }
+        else{
+          navigate("/setAvatar");
+        }
+      }
+    }
+    func();
+  },[]);
+
   return (
-    <div>Chat</div>
-  )
+    <Container>
+      <div className="container">
+        <Contacts contacts = {contacts} currentUser = {currentUser} />
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 
-const {register,login, setAvatar} = require("../controllers/usersControllers");
+const {register,login, setAvatar, getAllUsers} = require("../controllers/usersControllers");
 const router = require("express").Router();
 
 router.post("/register",register);
@@ -7,5 +7,7 @@ router.post("/register",register);
 router.post("/login",login);
 
 router.post("/setAvatar/:id", setAvatar);
+
+router.get("/allusers/:id", getAllUsers);
 
 module.exports = router;
